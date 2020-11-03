78857e393ffba9c9104ce5b5fabc6705fe2175b0
Tristen <stella@pop-os.localdomain>
2020-11-03 17:43:55 -0500

    Initial Commit

---
+++ kernel.cpp
@@ -0,0 +1,17 @@
+
+#include "types.h"
+
+void printf(char* str)
+{
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+    
+    for (int i = 0; str[i] != '\0'; ++i)
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+}
+
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
+{
+    printf("Hello world!");
+    
+    while(1);
+}
